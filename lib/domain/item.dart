2acf6335e41c334acbf7408e199d4055901247cd
brthema072 <brunomarthe@hotmail.com>
2023-08-01 12:57:15 -0300

    Class item e items

--- lib/domain/item.dart
+++ lib/domain/item.dart
@@ -1,6 +1,30 @@
+class Items {
+  List<Item> items;
+
+  Items(this.items);
+
+  factory Items.fromJson(Map<String, dynamic> json) {
+    List<Item> items = [];
+
+    json["items"].forEach((item) {
+      items.add(Item.fromJson(item));
+    });
+
+    return Items(items);
+  }
+
+  Map<String, dynamic> toJson() => {"items": items};
+}
+
 class Item {
   String name;
   bool isChecked;
 
   Item(this.name, this.isChecked);
+
+  factory Item.fromJson(Map<String, dynamic> json) {
+    return Item(json["name"], json["isChecked"]);
+  }
+
+  Map<String, dynamic> toJson() => {"name": name, "isChecked": isChecked};
 }
